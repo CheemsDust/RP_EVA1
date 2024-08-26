2a5f14586e8a1f712fe66d49334f6975a58eb628
CheemsDust <L24550259@chihuahua2.tecnm.mx>
2024-08-26 17:54:29 -0600

    Add files via upload

---
+++ EVA1_3_VARIABLES2/src/eva1_3_variables2/EVA1_3_VARIABLES2.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_variables2;
+
+/**
+ *
+ * @author calza
+ */
+public class EVA1_3_VARIABLES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Vehiculo:
+        String marca;
+        String modelo;
+        int año; //la Ñ en otros lenguajes no es aceptada
+        double precio;
+        
+        // ASIGNACIÓN
+        marca = "NISSAN";
+        modelo = "GTR";
+        año = 2008;
+        precio = 1000000.1;
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(año);
+        System.out.println(precio);
+    }
+    
+}
