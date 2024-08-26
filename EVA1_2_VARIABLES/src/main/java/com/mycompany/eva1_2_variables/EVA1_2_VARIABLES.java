4c3ec0c2bab4ab821cea4b60ce8fcc0f522c3d09
CheemsDust <L24550259@chihuahua2.tecnm.mx>
2024-08-26 15:06:10 -0600

    Add files via upload

---
+++ EVA1_2_VARIABLES/src/main/java/com/mycompany/eva1_2_variables/EVA1_2_VARIABLES.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_2_variables;
+
+/**
+ *
+ * @author calza
+ */
+public class EVA1_2_VARIABLES {
+
+    public static void main(String[] args) {
+                                           //TIPOS DE DATOS
+        System.out.println("Hello World!");//Cadena de texto
+        System.out.println(100);           //Entero
+        System.out.println(3.1416);        //Número de punto flotante
+        System.out.println('c');           //caracter
+        System.out.println(true);          //Valores lógicos (booleanos)
+        System.out.println(false);         //;--> FIN DE LA INSTRUCCION (NO OLVIDAR PONERLO)
+        
+        //enteros:
+        //int
+        //long
+        //short
+        
+        //numeros con parte decimal (flotantes):
+        //float
+        //double
+        
+        //cadenas de texto:
+        //string
+        
+        //caracteres:
+        //char
+        
+        //valores logicos:
+        //boolean
+        
+        //declaraciones de variables:
+        //sintaxis para declarar variables (reglas)
+        //tipo_de_dato nombre_de_la_variable;
+        String nombre;
+        String apellido;
+        String curp;
+        float promedio;
+    }
+}
